1a3b19c69148328e03dd3953233009d5f1eb903b
Rahat-Ul-Ain <rain.bese16seecs@seecs.edu.pk>
2020-11-22 10:09:57 +0500

    Update diagnosis.py

--- code/hybrid_LSTM/diagnosis.py
+++ code/hybrid_LSTM/diagnosis.py
@@ -1,3 +1,13 @@
+/***************************************************************************************
+*    Modified by: Rahat Ul Ain 
+*    for feature extraction from Deep and Shallow CNN 
+*    Original Source
+*    Title: auto-eeg-diagnosis-example
+*    Author: Robin T. Schirrmeister   
+*    Date: Oct 25, 2017
+*    Availability: https://github.com/robintibor/auto-eeg-diagnosis-example
+*
+***************************************************************************************/
 import logging
 import time
 from copy import copy
