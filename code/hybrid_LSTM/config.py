19af5c0315c053cfc0093e2833cd8c9329206ca2
Rahat-Ul-Ain <rain.bese16seecs@seecs.edu.pk>
2021-05-20 10:05:02 +0500

    Add files via upload

---
+++ code/hybrid_LSTM/config.py
@@ -0,0 +1,32 @@
+# There should always be a 'train' and 'eval' folder directly
+# below these given folders
+# Folders should contain all normal and abnormal data files without duplications
+data_folders = [
+    'dataset/normal',
+    'dataset/abnormal']
+n_recordings = None  # set to an integer, if you want to restrict the set size
+sensor_types = ["EEG"]
+n_chans = 21
+max_recording_mins = None # exclude larger recordings from training set
+sec_to_cut = 60  # cut away at start of each recording
+duration_recording_mins = 10  # how many minutes to use per recording#20earlier
+test_recording_mins = 10#20earlier
+max_abs_val = 800  # for clipping
+sampling_freq = 100
+divisor = 10  # divide signal by this
+test_on_eval = True  # test on evaluation set or on training set
+# in case of test on eval, n_folds and i_testfold determine
+# validation fold in training set for training until first stop
+n_folds = 10
+i_test_fold = 9
+shuffle = True
+model_name = 'shallow'#shallow/deep for DNN (deep terminal local 1)
+n_start_chans = 25
+n_chan_factor = 2  # relevant for deep model only
+input_time_length = 6000
+final_conv_length = 1
+model_constraint = 'defaultnorm'
+init_lr = 1e-3
+batch_size = 64
+max_epochs = 35 # until first stop, the continue train on train+valid
+cuda = True # False
